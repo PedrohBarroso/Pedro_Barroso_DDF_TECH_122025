06bb0e87ebc8048cc0ce063c5062dc062d4475a1
Pedro Henrique Barroso <ph.barroso02@gmail.com>
2025-12-31 22:48:42 -0300

    Correção Caminho absoluto Arquivos Scrit de conversão e Arquivos ReadMe

--- Docs/Etapa2/convert_parquet_csv.py
+++ Docs/Etapa2/convert_parquet_csv.py
@@ -5,10 +5,10 @@ LIMITE = 101000
 
 print("Carregando arquivo...")
 # O Pandas carrega o arquivo e o .head() seleciona apenas as primeiras linhas
-df = pd.read_parquet('yellow_tripdata_2025-01.parquet')
+df = pd.read_parquet('~/Documentos/case_tecnico/dataset/yellow_tripdata_2025-01.parquet')
 df_limitado = df.head(LIMITE)
 
 print(f"Salvando {LIMITE} registros em CSV...")
-df_limitado.to_csv('yellow_trip.csv', index=False, encoding='utf-8')
+df_limitado.to_csv('~/Documentos/case_tecnico/dataset/yellow_trip.csv', index=False, encoding='utf-8')
 
 print("Concluído!")
